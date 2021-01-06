3f8a9e412766978d6c1f52b86d707d665a371884
avasilei <73549541+avasilei@users.noreply.github.com>
2021-01-06 16:51:49 +0200

    Update BoardOperator.php

--- src/Operators/BoardOperator.php
+++ src/Operators/BoardOperator.php
@@ -1,1 +1,59 @@
+<?php
+
+use CodeOn\Backgammon\Model\Piece;
+
+class BoardOperator{
+
+    private $BoardConstructor;
+
+    private $PawnConstructor;
+
+    private $PositionConstructor;
+
+    private $board;
+
+    public function __construct(BoardConstructor $BoardConstructor, PositionConstructor  $PositionConstructor, PawnConstructor $PawnConstructor){
+        $this->BoardConstructor = $BoardConstructor;
+        $this->PositionConstructor = $PositionConstructor;
+        $this->PawnConstructor = $PawnConstructor;
+    }
+//create new round 
+    public function setNewRound(){
+        $this->board = $this->BoardConstructor->create();
+        $this->createBoardPositions();
+
+        $this->setPositionPieces();
+
+        $this->setPositionPieces(1, 15, Piece::COLOUR_WHITE);
+        $this->setPositionPieces(13, 15, Piece::COLOUR_BLACK);
+
+    }
+//create board 
+    public function createBoardPositions($index, $quantity, $pawncolor){
+        for ($i=0; $i< $quantity; $i++){
+            $this->board->getPosition($index)->addPawn($this->PawnConstructor->create($pawncolor));
+        }
+
+    }
+//stysimo poyliwn
+    public function setPositionPieces($index, $quantity, $pawncolor){
+        for($i=0; $i<quantity; $i++){
+            $this->board->getPosition($index)->addPawn($this->PawnConstructor->create($pawncolor));
+        }
+
+    }
+//kinhsh 
+    public function movePawn($positionIndex, $newPositionIndex){
+        $position = $this->board->getPotision($positionIndex);
+        $pawn = $position->removePawn();
+
+        $newPosition = $this->board->getPotision($newPositionIndex);
+
+        $newPosition->addPawn($pawn);
+    }
+//trexousa katastash board
+    public function getBoardState(){
+        return $this->board;
+    }
+}
 
