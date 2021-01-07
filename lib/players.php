515c1a08e2606cb1f8d52ca9c114c683f2755b82
avasilei <73549541+avasilei@users.noreply.github.com>
2021-01-07 02:56:09 +0200

    Create players.php

---
+++ lib/players.php
@@ -0,0 +1,13 @@
+<?php 
+
+public function players() {
+	global $mysqli;
+	$sql = 'select username,piece_color from players';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+?>
