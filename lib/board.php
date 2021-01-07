acecff3ab6d674b0e85413b409af2e99c56b0a10
avasilei <73549541+avasilei@users.noreply.github.com>
2021-01-07 02:34:38 +0200

    Update board.php

--- lib/board.php
+++ lib/board.php
@@ -1,4 +1,18 @@
 <?php
+//eisagwgh arxeiwn 
+require './src/Operators/BoardOperator.php';
+require './src/Operators/GameOperator.php';
+require './src/Constructors/BoardConstructor.php';
+require './src/Constructors/DiceConstructor.php';
+require './src/Constructors/PawnConstructor.php';
+require './src/Constructors/PositionConstructor.php';
+require './src/Constructors/MovementConstructor.php';
+require './src/Parts/Movement.php';
+require './src/Parts/Board.php';
+require './src/Parts/Position.php';
+require './src/Parts/Dice.php';
+require './src/Parts/Pawn.php';
+require './src/Parts/Players.php';
 //return current state of board in json format
 function currentBoard() {
    
